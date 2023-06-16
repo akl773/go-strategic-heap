91ea5f2b27fc12a25873a868cb8f5354a9cd8d4b
Akhil singh <akhil.singh@infra.market>
2023-06-16 14:47:14 +0530

    add heap strategies

--- heap.go
+++ heap.go
@@ -15,3 +15,16 @@ type MaxHeapStrategy struct{}
 func (MaxHeapStrategy) ShouldSwap(parent, child int) bool {
 	return parent < child
 }
+
+type Heap struct {
+	data     []int
+	strategy Strategy
+}
+
+func NewMinHeap() *Heap {
+	return &Heap{data: nil, strategy: MinHeapStrategy{}}
+}
+
+func NewMaxHeap() *Heap {
+	return &Heap{data: nil, strategy: MaxHeapStrategy{}}
+}
